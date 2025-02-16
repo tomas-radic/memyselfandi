5132e8de3894a4b6361c2570e384d9d039fda578
Tomas Radic <tomas.radic@gmail.com>
2025-02-16 07:21:06 +0100

    Updated content

--- spec/requests/profiles_spec.rb
+++ spec/requests/profiles_spec.rb
@@ -4,11 +4,21 @@ RSpec.describe "Profiles", type: :request do
   describe "GET /show" do
     subject { get "/profiles/show" }
 
-    it "Returns http success" do
+    it "Renders template" do
       subject
 
       expect(response).to render_template(:show)
       expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /download" do
+    subject { get "/profiles/download" }
+
+    it "Sends file" do
+      expect_any_instance_of(ProfilesController).to receive(:send_file).with('public/profile.pdf')
+
+      subject
     end
   end
 end
